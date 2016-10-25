66aed97473d8d7d7932c82be0a7689dbde2c8dc8
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2016-10-25 13:57:37 +0200

    Script to create feature vectors for clustering

---
+++ scripts/make_feature_vectors.py
@@ -0,0 +1,13 @@
+import glob
+import pystain
+from pystain import StainDataset, StainCluster
+
+fns = glob.glob('/home/gdholla1/data/post_mortem/new_data_format/*')
+
+subject_ids = [int(fn.split('/')[-1]) for fn in fns]
+
+
+for subject_id in subject_ids:
+    dataset = StainDataset(subject_id)
+    cluster = StainCluster(dataset)
+    cluster.make_feature_vector()
