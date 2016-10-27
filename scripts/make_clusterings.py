9a7e1cc6e471d0e52c879f9b07e40b3a75c5a8dc
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2016-10-27 13:42:43 +0200

    Updated clustering script

--- scripts/make_clusterings.py
+++ scripts/make_clusterings.py
@@ -1,10 +1,10 @@
-import pystain
-from pystain import StainDataset, StainCluster
-
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
+
+import pystain
+from pystain import StainDataset, StainCluster
 
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
@@ -12,11 +12,16 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 import pandas
 import numpy as np
 import seaborn as sns
+import os
+import pickle as pkl
+
+
+mask_thr = 0.5
 
 sns.set_style('whitegrid')
 
 
-subject_ids = [13054, 13061, 13072, 13074, 13095, 14037, 14051, 14069, 15033, 15035, 15052, 15055]
+subject_ids = [15055, 13054, 13061, 13072, 13074, 13095, 14037, 14051, 14069, 15033, 15035, 15052]
 #subject_ids = [15055]
 
 max_n_components = 5
@@ -25,6 +30,11 @@ for subject_id in subject_ids:
     for fwhm in [0.15, 0.3]:
 
         print subject_id, fwhm
+
+        d = '/home/gdholla1/data/post_mortem/clusterings_v1/%s_%s/' % (subject_id, fwhm)
+
+        if not os.path.exists(d):
+            os.makedirs(d)
 
         try:
 
@@ -35,12 +45,23 @@ for subject_id in subject_ids:
             incomplete_slices = cluster.get_incomplete_slices()
             cluster.drop_slices(incomplete_slices)
             cluster.normalize_feature_vector()
+
+
+            fn = os.path.join(d, 'incomplete_slices.txt')
+            f = open(fn, 'w')
+            f.writelines([' * %d\n\r' % e for e in incomplete_slices])
+            f.close()
             
 
             cluster.cluster_gmm(range(1, max_n_components+1))
+            
+            fn = os.path.join(d, 'gmms.pkl')
+            pkl.dump(cluster.gmms, open(fn, 'w'))
+
             bics = [gmm.bic(cluster.feature_vector) for gmm in cluster.gmms]
             
-            fn = '/home/gdholla1/data/post_mortem/clusterings_v1/%s_%s.pdf' % (subject_id, fwhm)
+            fn = os.path.join(d, 'clusters.pdf')
+
             pdf = PdfPages(fn)
             
             for n_components in xrange(1, max_n_components+1):
@@ -51,21 +72,34 @@ for subject_id in subject_ids:
                 plt.close(plt.gcf())
 
                 for i in xrange(1, n_components+1):
+                    print "Plotting component %d" % i
 
                     for j, q in enumerate([0.25, 0.5, 0.75]):
-                        print j, q
                         slice_coronal = cluster.get_proportional_slice(q)
-                        slice_axial = cluster.get_proportional_slice(1-q, 'axial')
+                        slice_axial = cluster.get_proportional_slice(q, 'axial')
                         slice_sagittal = cluster.get_proportional_slice(q, 'sagittal')  
 
                         plt.subplot(3,3,(3*j)+1)
-                        cluster.plot_cluster_probability(component=i, n_components=n_components,slice=slice_coronal, mask_thr=1e-12)
+                        cluster.plot_cluster_probability(component=i, n_components=n_components,slice=slice_coronal, mask_thr=mask_thr)
+                        xticks = np.arange(0, np.ceil(cluster.get_x_coordinate(cluster.stain_dataset.xlim[1])))
+                        plt.xticks(xticks)
+                        yticks = np.arange(0, np.ceil(cluster.get_z_coordinate(cluster.stain_dataset.zlim[0])))
+                        plt.yticks(yticks)
 
                         plt.subplot(3,3,(3*j)+2)
-                        cluster.plot_cluster_probability('axial', component=i, n_components=n_components,slice=slice_axial, mask_thr=1e-12)        
+                        cluster.plot_cluster_probability('axial', component=i, n_components=n_components,slice=slice_axial, mask_thr=mask_thr)        
+                        xticks = np.arange(0, np.ceil(cluster.get_x_coordinate(cluster.stain_dataset.xlim[1])))
+                        plt.xticks(xticks)
+                        yticks = np.arange(0, np.ceil(cluster.get_slice_coordinate(cluster.slices[0])))
+                        plt.yticks(yticks)
 
                         plt.subplot(3,3,(3*j)+3)
-                        cluster.plot_cluster_probability('sagittal', component=i, n_components=n_components,slice=slice_sagittal, mask_thr=1e-12)                
+                        cluster.plot_cluster_probability('sagittal', component=i, n_components=n_components,slice=slice_sagittal, mask_thr=mask_thr)                
+
+                        xticks = np.arange(0, np.ceil(cluster.get_slice_coordinate(cluster.slices[0])))
+                        plt.xticks(xticks)
+                        yticks = np.arange(0, np.ceil(cluster.get_z_coordinate(cluster.stain_dataset.zlim[0])))
+                        plt.yticks(yticks)
 
                 plt.gcf().set_size_inches(25, 25)
 
